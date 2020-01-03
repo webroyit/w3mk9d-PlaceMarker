1f8d5f62d9563b1d651406a458b7311dfec53720
royweb <xytank@gmail.com>
2020-01-03 12:55:45 -0500

    Use body parser and enable CORS

--- server.js
+++ server.js
@@ -8,6 +8,16 @@ dotenv.config({ path: "./config/config.env" });
 
 const app = express();
 
+// body parser
+app.use(express.json());
+
+// enable cors
+app.use(cors());
+
+app.get('/api/v1/places', (req, res) => {
+    res.send("It works");
+})
+
 const PORT = process.env.PORT || 1000;
 
 app.listen(PORT, () => console.log(`Your server is in ${process.env.NODE_ENV} on port ${PORT}`));
