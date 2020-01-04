ca43a7b83ea82383ed0098aec2e1e48bd64582a2
royweb <xytank@gmail.com>
2020-01-04 12:35:04 -0500

    Update the getPlaces function to fetch the data from the database

--- controllers/places.js
+++ controllers/places.js
@@ -2,6 +2,18 @@ const Place = require("../models/Places");
 
 // GET /api/v1/places
 // Public
-exports.getPlaces = (req, res, next) => {
-    res.send("It works");
+exports.getPlaces = async (req, res, next) => {
+    try{
+        const places = await Place.find();
+
+        return res.status(200).json({
+            success: true,
+            // it get the data in an array
+            count: places.length,
+            data: places
+        })
+    }catch(err){
+        console.log(err);
+        res.status(500).json({ error: "Something went wrong in the server"})
+    }
 }
