48f68ac077aa7205524c4fd1331c2ad9bfa43998
royweb <xytank@gmail.com>
2020-01-04 13:15:20 -0500

    Add a route that create a new place

--- controllers/places.js
+++ controllers/places.js
@@ -14,6 +14,27 @@ exports.getPlaces = async (req, res, next) => {
         })
     }catch(err){
         console.log(err);
-        res.status(500).json({ error: "Something went wrong in the server"})
+        // 500 is server error
+        res.status(500).json({ error: "Something went wrong in the server"});
+    }
+}
+
+// POST /api/v1/places
+// Public
+exports.addPlace = async (req, res, next) => {
+    try{
+       const place = await Place.create(req.body);
+
+       return res.status(200).json({
+            success: true,
+            data: place
+       })
+    }catch(err){
+        console.log(err);
+        if(err.code === 11000){
+            // 400 is user error
+            return res.status(400).json({ error: "This place already exists"});
+        }
+        res.status(500).json({ error: "Something went wrong in the server"});
     }
 }
--- routes/places.js
+++ routes/places.js
@@ -1,8 +1,11 @@
 const express = require("express");
-const { getPlaces } = require("../controllers/places");
+const { getPlaces, addPlace } = require("../controllers/places");
 
 const router = express.Router();
 
-router.route("/").get(getPlaces);
+router
+    .route("/")
+    .get(getPlaces)
+    .post(addPlace);
 
 module.exports = router;
