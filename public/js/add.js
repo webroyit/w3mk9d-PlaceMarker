0768847cae22d3382447a4dc0dd0e835a01c7f66
royweb <xytank@gmail.com>
2020-01-06 13:29:13 -0500

    Add a function that add a new place

--- public/js/add.js
+++ public/js/add.js
@@ -4,11 +4,41 @@ const placeId = document.getElementById("place-id");
 const placeAddress = document.getElementById("place-address");
 
 // send place data to the server
-function addPlace(e){
+async function addPlace(e){
     e.preventDefault();
 
+    // validation
     if(placeId.value === "" || placeAddress.value === ""){
         alert("Please fill in the fields");
+    }
+
+    const sendBody = {
+        placeId: placeId.value,
+        address: placeAddress.value
+    }
+
+    try{
+        const res = await fetch("/api/v1/places", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(sendBody)
+        });
+
+        if(res.status === 400){
+            // this will go to catch block
+            throw Error("Place already exist");
+        }
+
+        alert("Place is added");
+
+        // redirect to home page
+        window.location.href = "/index.html";
+
+    }catch(err){
+        alert(err);
+        return;
     }
 }
 
